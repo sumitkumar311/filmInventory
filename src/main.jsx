55c697d4830668fee7f7fd984ac85eee3fba209c
Sumit Kumar <sumitshrivaa44@gmail.com>
2025-05-10 09:58:49 +0530

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { store } from './store/store.jsx'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
       <Provider store={store}>
-    <BrowserRouter  basename="/filmInventory" >
+    <Router >
       <App />
-    </BrowserRouter>
+    </Router>
     </Provider>
   </StrictMode>
 );
